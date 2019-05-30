4420f231cc0f4c7d59cba82f8d99dbcd61db9462
Ming Liu <mliu@macpro.home>
2019-05-30 15:15:36 -0400

    made change to package.json

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const server = require('./server.js');
 
-server.listen(8800, () => {
-  console.log('\n* Server Running on http://localhost:8800 *\n');
+const port = process.env.PORT || 8800;
+
+server.listen(port, () => {
+  console.log(`  Server Running on http://localhost:${port}  `);
 });
 
